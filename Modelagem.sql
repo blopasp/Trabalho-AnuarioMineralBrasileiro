5a0343b5ff32960f63cb2812bee152e9cab0d887
blopasp <77591670+blopasp@users.noreply.github.com>
2021-05-10 23:35:58 -0300

    Update Modelagem.sql

--- Modelagem.sql
+++ Modelagem.sql
@@ -1,31 +1,31 @@
 use AnuarioMineralBrasileiro;
 
 CREATE TABLE IF NOT EXISTS Dim_Regiao(
-	id_Regiao smallint PRIMARY KEY auto_increment
+    id_Regiao smallint PRIMARY KEY auto_increment
     ,UF varchar(2) unique NOT NULL
     ,Estado varchar(50) unique NOT NULL
 ); 
 CREATE TABLE IF NOT EXISTS Dim_Calendar(
-	id_Calendar smallint PRIMARY KEY auto_increment
+    id_Calendar smallint PRIMARY KEY auto_increment
     ,Data Date not null
     ,Dia varchar(02) not null
     ,Mes varchar(02) not null
     ,Ano smallint not null
 );
 CREATE TABLE IF NOT EXISTS Dim_UnidadeMedida(
-	id_UnidadeMedida int primary key auto_increment
+    id_UnidadeMedida int primary key auto_increment
     ,Unidade varchar(2) UNIQUE NOT NULL
     ,Descricao varchar(20) NOT NULL
     ,RelacaoQuilo float NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Dim_SubstanciaMineral(
-	id_SubstanciaMineral int primary key auto_increment
+    id_SubstanciaMineral int primary key auto_increment
     ,SubstanciaMineral varchar(255) UNIQUE NOT NULL
     ,ClasseSubstancia  varchar(50) NOT NULL
     ,IndicacaoContida varchar(50) 
 );
 CREATE TABLE IF NOT EXISTS Fato_ProdutoBeneficiado(
-	id_Calendar smallint not null
+    id_Calendar smallint not null
     ,id_Regiao smallint not null
     ,id_SubstanciaMineral int not null
     ,id_UnidadeMedida int not null
